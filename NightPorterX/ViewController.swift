3fead85d6750562b9ebe40819bda6a76a358ecb4
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-06-30 12:02:48 -0500

    Initial Commit

---
+++ NightPorterX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NightPorterX
+//
+//  Created by Miguel Rueda on 30/06/20.
+//  Copyright © 2020 Miguel Rueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
