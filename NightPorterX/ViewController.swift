a55e0d531a2582745e0b951c8f30e283c39b6e36
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-06-30 18:34:58 -0500

    Updated UI

--- NightPorterX/ViewController.swift
+++ NightPorterX/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dailyTaskLabel: UILabel!
+    var clickedTimes = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,6 +20,19 @@ class ViewController: UIViewController {
 
     @IBAction func changeBackground(_ sender: Any) {
         view.backgroundColor = UIColor.darkGray
+        
+        // TODO: Change the text color of all the labels
+        // Get everything contained in the top-level view
+        let everything = view.subviews
+        for item in everything {
+            if item is UILabel {
+                // downcast as UILabel
+                let currentLabel = item as! UILabel
+                currentLabel.textColor = UIColor.lightGray
+            }
+        }
+        clickedTimes += 1
+        dailyTaskLabel.text = "Clicked \(clickedTimes)"
     }
     
 }
