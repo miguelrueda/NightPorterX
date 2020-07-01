2ba3361a33298e71d22a8cafbb3450b9164f69c2
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-07-01 11:10:08 -0500

    Added table View

--- NightPorterX/ViewController.swift
+++ NightPorterX/ViewController.swift
@@ -8,32 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var dailyTaskLabel: UILabel!
-    var clickedTimes = 0
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // Table View Delegate Methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected \(indexPath.row) in section \(indexPath.section)")
+    }
+    
+    let dailyTasks = [
+        "Check all windows",
+        "Check all doors",
+        "Is the boiler fueled?",
+        "Check the mailbox",
+        "Empty trash containers",
+        "If freezing, check water pipes",
+        "Document \"strange and unusual\" occurrences",
+    ]
+    
+    let weeklyTasks = [
+        "Check inside all cabins",
+        "Flush all lavatories in cabins",
+        "Walk the perimeter of property",
+    ]
+    
+    let monthlyTasks = [
+        "Test security alarm",
+        "Test monitor detectors",
+        "Test smoke alarms",
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func changeBackground(_ sender: Any) {
-        view.backgroundColor = UIColor.darkGray
-        
-        // TODO: Change the text color of all the labels
-        // Get everything contained in the top-level view
-        let everything = view.subviews
-        for item in everything {
-            if item is UILabel {
-                // downcast as UILabel
-                let currentLabel = item as! UILabel
-                currentLabel.textColor = UIColor.lightGray
-            }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return dailyTasks.count
+        case 1:
+            return weeklyTasks.count
+        case 2:
+            return monthlyTasks.count
+        default:
+            return 0
         }
-        clickedTimes += 1
-        dailyTaskLabel.text = "Clicked \(clickedTimes)"
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = dailyTasks[indexPath.row]
+        case 1:
+            cell.textLabel?.text = weeklyTasks[indexPath.row]
+        case 2:
+            cell.textLabel?.text = monthlyTasks[indexPath.row]
+        default:
+            cell.textLabel?.text = "This souldn't happen"
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        case 2:
+            return "Monthly Tasks"
+        default:
+            return nil
+        }
     }
     
 }
-
